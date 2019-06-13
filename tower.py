b83327e277062cc618ddea27a0db082258d3cbb0
etermaw <wuwrut@gmail.com>
2019-06-13 15:57:40 +0200

    fixes in tower

--- tower.py
+++ tower.py
@@ -1,39 +1,18 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as func
+import torch.nn.functional as F
 
 
 class Tower(nn.Module):
     def __init__(self):
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 256, kernel_size=2, stride=2),
-            nn.ReLU()
-        )
+        super(Tower, self).__init__()
 
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=3, padding=1),
-            nn.ReLU()
-        )
-
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(384, 256, kernel_size=2, stride=2),
-            nn.ReLU()
-        )
-
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(256+7, 128, kernel_size=3, padding=1),
-            nn.ReLU()
-        )
-
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(391, 256, kernel_size=3, padding=1),
-            nn.ReLU()
-        )
-
-        self.layer6 = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=1),
-            nn.ReLU()
-        )
+        self.layer1 = nn.Conv2d(3, 256, kernel_size=2, stride=2)
+        self.layer2 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+        self.layer3 = nn.Conv2d(384, 256, kernel_size=2, stride=2)
+        self.layer4 = nn.Conv2d(256+7, 128, kernel_size=3, padding=1)
+        self.layer5 = nn.Conv2d(391, 256, kernel_size=3, padding=1)
+        self.layer6 = nn.Conv2d(256, 256, kernel_size=1)
 
     def forward(self, x, view):
         v1 = view.unsqueeze(-1)
@@ -42,17 +21,17 @@ class Tower(nn.Module):
         v3 = v2.unsqueeze(-1)
         v4 = v3.expand((5, 7, 16, 16))
 
-        x = self.layer1(x)
-        y = self.layer2(x)
+        x = F.relu(self.layer1(x))
+        y = F.relu(self.layer2(x))
         x = torch.cat([x, y], dim=1)
 
-        x = self.layer3(x)
+        x = F.relu(self.layer3(x))
 
         x = torch.cat([x, v4], dim=1)
-        y = self.layer4(x)
+        y = F.relu(self.layer4(x))
         x = torch.cat([x, y], dim=1)
 
-        x = self.layer5(x)
-        x = self.layer6(x)
+        x = F.relu(self.layer5(x))
+        x = F.relu(self.layer6(x))
 
         return x.sum(0)
