b796a584a03aea00c019aed0b0dfd22f577c4c17
Tomasz Pecyna <tomasz.pecyna@tomtom.com>
2019-06-13 15:31:17 +0200

    cleaning

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from loader import GQNDataset
 import matplotlib.pyplot as plt
 import numpy as np
-from QGN import GQN
+from GQN import GQN
 
 BATCH_SIZE = 32
 N = 5000
@@ -14,22 +14,14 @@ if __name__ == '__main__':
 
 
     ds = GQNDataset(mode='train', base_dir='data', scene='rooms_ring_camera')
-    x, pos = ds[0]
+    cameras, positions = ds[0]
 
     qgn = GQN(shared=shared, L=L)
 
-    n = 6
-    f = plt.figure(figsize=(12, 8))
-    axes = f.subplots(nrows=n, ncols=1, sharex=True, sharey=True)
     for i in range(0, N, BATCH_SIZE):
-        view_batch, pos_batch = x[i:i+BATCH_SIZE], pos[i:i+BATCH_SIZE]
+        view_batch, pos_batch = cameras[i:i+BATCH_SIZE], positions[i:i+BATCH_SIZE]
         x = view_batch[:,0:5, ...]
         p = pos_batch[:,0:5, ...]
         x_q = view_batch[:, -1, ...]
         p_q = pos_batch[:, -1, ...]
         qgn(x, p, x_q, p_q)
-        #grid = np.hstack(images[:10])
-        #axes[i].imshow(grid)
-
-    plt.show()
-    a = 15
